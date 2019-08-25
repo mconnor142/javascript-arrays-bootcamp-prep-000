121d22c0a66d2cab1ccc59d96b8d1889b559fb71
Maxwell Connor <mconnor142@gmail.com>
2019-08-25 17:21:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, item){
+<<<<<<< HEAD
   return [item, ...array];
+=======
+  var newArray = array;
+  newArray.unshift(item);
+  return newArray;
+>>>>>>> 91767764b2c8bfc246f5f3c88cd7b0b6b9a22fbc
 }
 
 function destructivelyAddElementToBeginningOfArray(array, item){
@@ -10,12 +16,21 @@ function destructivelyAddElementToBeginningOfArray(array, item){
 }
 
 function addElementToEndOfArray(array, item){
+<<<<<<< HEAD
  return [...array, item];
+=======
+  var newArray = array;
+  newArray.push(item);
+  return newArray;
+>>>>>>> 91767764b2c8bfc246f5f3c88cd7b0b6b9a22fbc
 }
 
 function destructivelyAddElementToEndOfArray(array, item){
   array.push(item);
+<<<<<<< HEAD
   return array;
+=======
+>>>>>>> 91767764b2c8bfc246f5f3c88cd7b0b6b9a22fbc
 }
 
 function accessElementInArray(array, index){
@@ -32,8 +47,12 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
+<<<<<<< HEAD
   array.pop();
   return array;
+=======
+  return array.pop();
+>>>>>>> 91767764b2c8bfc246f5f3c88cd7b0b6b9a22fbc
 }
 
 function removeElementFromEndOfArray(array){
